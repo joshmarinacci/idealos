431df39738d80c3dd517d6121f8de9228244bfb4
josh <joshua@marinacci.org>
2020-06-18 16:05:37 -0700

    add attr in array

--- proto2/album_test.js
+++ proto2/album_test.js
@@ -54,34 +54,6 @@ const Artist = {
     }
 }
 
-const Track = {
-    meta: {
-        name: 'Track',
-    },
-    track_number: {
-        required:false,
-        type: Integer,
-        dummy: () => rand_int_range(1,20),
-        ui: { type: inputs.Number, columns: 2, label:'Track No' },
-    },
-    artist: {
-        type: Artist,
-        required: false,
-        ui: { type: inputs.Dropdown_Search, label: 'Artist'},
-    },
-    title: {
-        type: String,
-        required: false,
-        dummy: () => words.pick(4).join(" "),
-        ui: { type: inputs.Text, rows:1},
-    },
-    year: {
-        type: Year,
-        required:false,
-        ui: { type: inputs.YearPicker, label:'Year'},
-    }
-}
-
 const Album = {
     meta: {
         name: 'Album',
@@ -107,6 +79,38 @@ const Album = {
     }
 }
 
+const Track = {
+    meta: {
+        name: 'Track',
+    },
+    track_number: {
+        required:false,
+        type: Integer,
+        dummy: () => rand_int_range(1,20),
+        ui: { type: inputs.Number, columns: 2, label:'Track No' },
+    },
+    artist: {
+        type: Artist,
+        required: false,
+        ui: { type: inputs.Dropdown_Search, label: 'Artist'},
+    },
+    album: {
+        type: Album,
+        required: false,
+    },
+    title: {
+        type: String,
+        required: false,
+        dummy: () => words.pick(4).join(" "),
+        ui: { type: inputs.Text, rows:1},
+    },
+    year: {
+        type: Year,
+        required:false,
+        ui: { type: inputs.YearPicker, label:'Year'},
+    }
+}
+
 const DB = {
     OBJS:[],
     clear_all() {
@@ -127,7 +131,7 @@ function create(type, opts) {
             throw new Error(`cannot make type ${type.meta.name}, missing required ${key}`)
         }
         if(key in opts) {
-            console.log("setting",key)
+            // console.log("setting",key)
             obj[key] = opts[key]
         }
     })
@@ -142,6 +146,10 @@ function create_dummy(type, opts) {
     let obj = {
         type: type,
     }
+    // console.log(`Making instance of type ${type.meta.name}`)
+    Object.keys(opts).forEach(key => {
+        if(!type[key]) throw new Error(`can't set attr ${key} on type ${type.meta.name}`)
+    })
     Object.keys(type).forEach(key => {
         if(key === 'meta') return
         let def = type[key]
@@ -149,16 +157,15 @@ function create_dummy(type, opts) {
         // console.log(`def for ${key} is`,def)
         if(def.dummy) {
             obj[key] = def.dummy()
-            console.log(`generate dummy ${type.meta.name}.${key} := "${obj[key]}"`)
+            // console.log(`generate dummy ${type.meta.name}.${key} := "${obj[key]}"`)
         } else {
             if(def.type.dummy) {
                 // console.log("def type has a dummy")
                 obj[key] = def.type.dummy()
-                console.log(`generate dummy ${type.meta.name}.${key} := "${obj[key]}"`)
+                // console.log(`generate dummy ${type.meta.name}.${key} := "${obj[key]}"`)
             }
         }
         if(key in opts) {
-            console.log("overriding ",key)
             obj[key] = opts[key]
         }
     })
@@ -183,18 +190,18 @@ const check = {
 
 function simple_test() {
     let song1 = create(Track,{track_number:3})
-    check.dump(song1)
+    // check.dump(song1)
     check.equals(song1.get('track_number'),3)
     let song2 = create_dummy(Track)
     check.not_null(song2)
-    check.dump(song2)
+    // check.dump(song2)
     check.not_null(song2.get('year'))
 
     let beatles = create(Artist, {title:'The Beatles'})
-    check.dump(beatles)
+    // check.dump(beatles)
 
     let song3 = create_dummy(Track, { artist: beatles})
-    check.dump(song3)
+    // check.dump(song3)
 }
 
 
@@ -221,7 +228,7 @@ class LiveQuery {
     current() {
         let res = DB.OBJS.filter(obj => {
             if(this.opts.type) {
-                if(obj.type.name !== this.opts.type.name) return false
+                if(obj.type.meta.name !== this.opts.type.meta.name) return false
             }
             let atts = Object.keys(this.opts.attrs);
             for(let i=0; i<atts.length; i++) {
@@ -233,13 +240,17 @@ class LiveQuery {
                     // console.log(`comparing ${obj[key]} ${attr_filter.type} ${attr_filter.value}`)
                     if (obj[key] !== attr_filter.value) return false
                 }
+                if(attr_filter.type === 'in-array') {
+                    console.log(`checking if ${obj[key]} is in ${attr_filter.value}`)
+                    if(attr_filter.value.indexOf(obj[key])<0) return false
+                }
             }
             return true
         })
         return new LiveQueryResults(res)
     }
     sync() {
-        console.log("SYNC")
+        // console.log("SYNC")
     }
 }
 
@@ -262,9 +273,16 @@ class LiveQueryBuilder {
         }
         return this
     }
+    attr_in_array(attr_name, value) {
+        this.attrs[attr_name] = {
+            type:'in-array',
+            value:value
+        }
+        return this
+    }
 }
 
-function create_live_query(type) {
+function create_live_query() {
     return new LiveQueryBuilder()
 }
 
@@ -288,7 +306,7 @@ function query_test() {
     all_albums_query.sync()
     check.equals(all_albums_query.current().length(),4)
 
-    let yellowsub_query = create_live_query(Album).attr_eq("title","Yellow Submarine").make()
+    let yellowsub_query = create_live_query().all(Album).attr_eq("title","Yellow Submarine").make()
     yellowsub_query.sync()
     all_albums_query.sync()
     check.equals(all_albums_query.current().length(),4, 'total album count')
@@ -299,17 +317,65 @@ function query_test() {
     all_albums_query.sync()
     check.equals(all_albums_query.current().length(),5, 'total album count')
     check.equals(yellowsub_query.current().length(),1)
+
+
+    // make three tracks. one on album1 and two on album 2
+    create_dummy(Track, {album:album1, title:'title1'})
+    create_dummy(Track, {album:album2, title:'title2'})
+    create_dummy(Track, {album:album2, title:'title3'})
+
+    // make a track query. verify three tracks total
+    let all_tracks_query = create_live_query().all(Track).make()
+    all_tracks_query.sync()
+    check.equals(all_tracks_query.current().length(),3)
+
+    // make a track query for tracks on album1, verify 1 track
+    let album1_tracks_query = create_live_query().all(Track).attr_eq('album',album1).make()
+    check.equals(album1_tracks_query.current().length(),1)
+
+
+    // make a track query for tracks on album2, verify 2 tracks
+    let album2_tracks_query = create_live_query().all(Track).attr_eq('album',album2).make()
+    check.equals(album2_tracks_query.current().length(),2)
+    //check the titles
+    check.equals(create_live_query().all(Track).attr_eq('title','title1').make().current().length(),1)
+    check.equals(create_live_query().all(Track).attr_eq('title','title2').make().current().length(),1)
+    //this one should fail
+    check.equals(create_live_query().all(Track)
+        .attr_eq('title','title4').make()
+        .current().length(),0)
+
+    //check if attr in array
+    console.log("checking if title in [title1,title2]")
+    check.equals(create_live_query().all(Track)
+        .attr_in_array('title',['title1','title2','title4']).make()
+        .current().length(),2)
+
+    //check if track in one of a set of albums
+    check.equals(create_live_query().all(Track)
+        .attr_in_array('album',[album1,album2]).make()
+        .current().length(),3)
+
+    /*
+    // make a Selection which contains an array of Albums
+    let selected_albums = create_dummy(Selection, {albums:[album2, album3]})
+
+    // make a query of albums in the selection
+    let selected_albums_query = create_live_query().all(Album).in_array(selected_albums).make()
+    check.equals(selected_albums_query.current().length(),2)
+
+    // make a query of tracks in albums in selection
+    let selected_tracks_query = create_live_query().all(Track).attr_eq('album',selected_albums_query).make()
+    check.equals(selected_tracks_query.current().length(),2)
+    */
 }
 
 function run_tests() {
     let args = Array.prototype.slice.call(arguments);
     try {
-        args.forEach(f => {
-            console.log(f)
-            f()
-        })
+        args.forEach(f => f())
     } catch (e) {
-        // console.log(e)
+        console.log(e)
         console.log("ERROR ",e.message)
     }
 }
